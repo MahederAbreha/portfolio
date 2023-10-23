0ebb61029388d839e6c40f2146a785c6b3986afc
mahi <maheder.abreha@miu.edu>
2023-10-23 02:20:23 -0500

    refactored the album

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Typography, Card, CardContent, Grid, Box } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
+import { Typography, Card, CardContent, Grid } from '@material-ui/core';
 
 const logos = [
     {src:'aws-2.svg', label: 'Aws'},
@@ -14,21 +13,22 @@ const logos = [
     {src:'mongodb-icon-1.svg', label: 'MongoDB'},
     {src:'docker.svg', label: 'Docker'}
 ];
-
+const iconSize = '15rem';
 const cardStyle = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
     borderRadius: '50%',
-    width: '10rem',
-    height: '10rem',
+    width: iconSize,
+    height: iconSize,
+    padding: '1rem',
 };
 const Content = ({label, src}) => {
     return (
-        <Card elevation={4} style={cardStyle} >
+        <Card elevation={20} style={cardStyle} >
             <object type="image/svg+xml" data={src} height="50%" width="50%"/>
-            <CardContent sx={{ flexGrow: 1 }} style={{ display: 'flex', justifyContent: 'center', padding: '0' }}>
+            <CardContent sx={{ flexGrow: 1 }} style={{ display: 'flex', justifyContent: 'center', padding: '0', }}>
                 <Typography gutterBottom variant="h5" component="h2">
                     { label }
                 </Typography>
@@ -36,13 +36,14 @@ const Content = ({label, src}) => {
         </Card>
     );
 };
+const centered = {display: 'flex', justifyContent:'center', alignItems:'center'};
 const Skills = () => {
     return (
         <>
             <div style={{ height: '10vh'}}/>
-            <Grid container spacing={5} >
+            <Grid container spacing={5} style={{ ...centered,  }}>
                 {logos.map((logo) => (
-                    <Grid item xs={6} sm={6} md={3} key={logo.label}>
+                    <Grid item xs={6} sm={6} md={3} key={logo.label}  style={{ ...centered, }}>
                         <Content label={logo.label} src={logo.src} />
                     </Grid>
                 ))}
@@ -50,5 +51,4 @@ const Skills = () => {
         </>
     );
 }
-
 export default Skills;
