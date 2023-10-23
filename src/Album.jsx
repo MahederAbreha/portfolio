8559832b1c00a32dd68b1232e65ec0d90c3ba275
mahi <maheder.abreha@miu.edu>
2023-10-23 03:33:40 -0500

    added email skeleton'

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,49 +1,76 @@
-import React from 'react';
-import { Container, Typography, Card, CardContent, Grid, ThemeProvider, createTheme, Box } from '@material-ui/core';
- 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import React, { useState } from 'react';
+import { Container, Typography, Card, CardContent, Grid, ThemeProvider, createTheme, Box } from '@mui/material';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 const defaultTheme = createTheme();
+
 export default function Album() {
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
+  const images = [
+    { src:'/album/photo1.jpg', heading:'Geode Park', desc:'something nice to be said about the picture' },
+    { src:'/album/photo2.jpg', heading:'Bayfront', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (1).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (2).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (3).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (4).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (5).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (6).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (7).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (8).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (9).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (10).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (11).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+    { src:'/album/photo (12).jpg', heading:'some place', desc:'something more nice to be said about this one too' },
+  ];
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      <main>
-        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }} >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Photography
-            </Typography>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <Box><img src='https://picsum.photos/300/300' /></Box>
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+      <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }} style={{padding: '1rem 0'}}>
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            Photography
+          </Typography>
         </Container>
-      </main>
+      </Box>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4} style={{ marginTop: '-6rem'}}>
+          {images.map((image, index) => (
+            <Grid item key={image} xs={12} sm={6} md={4}>
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', cursor: 'pointer' }} onClick={() => { setLightboxOpen(true); setPhotoIndex(index); }}>
+                <Box><img src={image.src} alt={`Image ${index + 1}`} style={{ height: '10rem', width: '17.78rem'}} /></Box>
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    { image.heading }
+                  </Typography>
+                  <Typography>
+                    { image.desc }
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+
+      {lightboxOpen && (
+        <Lightbox
+          mainSrc={images[photoIndex].src}
+          nextSrc={images[(photoIndex + 1) % images.length].src}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length].src}
+          onCloseRequest={() => setLightboxOpen(false)}
+          onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
+          onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
+        />
+      )}
     </ThemeProvider>
   );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Container, Typography } from '@material-ui/core';
 function Contact() {
     return (
-        <Container>
-            <Typography 
-            variant='h5'
-            align='center'
-            color='textSecondary'
-            gutterBottom
-            paragraph
-            >
-                contact
-            </Typography>
-        </Container>
+        // <Container>
+        //     <Typography 
+        //     variant='h5'
+        //     align='center'
+        //     color='textSecondary'
+        //     gutterBottom
+        //     paragraph
+        //     >
+        //         contact
+        //     </Typography>
+        // </Container>
+        <div>
+            <form action="https://formsubmit.co/5c289225fcf9a1787c740e435d9b6e11" method="POST">
+                <input type='email' name='email' placeholder='email' required/>
+                <input type='text' name='name' required placeholder='name' />
+                <textarea name='message' placeholder='message' required> </textarea>
+                <button type='submit'> Boom </button>
+            </form>
+        </div>
     );
 }
 
