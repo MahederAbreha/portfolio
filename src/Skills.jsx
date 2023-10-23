e8f97eeefaff362089a8518b3e02a532c421c73a
mahi <maheder.abreha@miu.edu>
2023-10-23 12:54:25 -0500

    refactored skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Typography, Card, CardContent, Grid } from '@material-ui/core';
+import { Typography, Card, CardContent, Grid, styled } from '@material-ui/core';
 
 const logos = [
-    {src:'aws-2.svg', label: 'Aws'},
-    {src:'java-4.svg', label: 'Java'},
-    {src:'kafka.svg', label: 'Kafka'},
-    {src:'logo-javascript.svg', label: 'Javascript'},
-    {src:'mysql-logo.svg', label: 'MySQL'},
-    {src:'react-2.svg', label: 'React'},
-    {src:'spring-3.svg', label: 'Spring'},
-    {src:'postgresql.svg', label: 'PostgreSQL'},
-    {src:'mongodb-icon-1.svg', label: 'MongoDB'},
-    {src:'docker.svg', label: 'Docker'}
+    { src: 'aws-2.svg', label: 'Aws' },
+    { src: 'java-4.svg', label: 'Java' },
+    { src: 'kafka.svg', label: 'Kafka' },
+    { src: 'logo-javascript.svg', label: 'Javascript' },
+    { src: 'mysql-logo.svg', label: 'MySQL' },
+    { src: 'react-2.svg', label: 'React' },
+    { src: 'spring-3.svg', label: 'Spring' },
+    { src: 'postgresql.svg', label: 'PostgreSQL' },
+    { src: 'mongodb-icon-1.svg', label: 'MongoDB' },
+    { src: 'docker.svg', label: 'Docker' }
 ];
-const iconSize = '15rem';
+const iconSize = '10rem';
 const cardStyle = {
     display: 'flex',
     justifyContent: 'center',
@@ -23,32 +23,28 @@ const cardStyle = {
     width: iconSize,
     height: iconSize,
     padding: '1rem',
-};
-const Content = ({label, src}) => {
-    return (
-        <Card elevation={20} style={cardStyle} >
-            <object type="image/svg+xml" data={src} height="50%" width="50%"/>
-            <CardContent sx={{ flexGrow: 1 }} style={{ display: 'flex', justifyContent: 'center', padding: '0', }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                    { label }
-                </Typography>
-            </CardContent>
-        </Card>
-    );
-};
-const centered = {display: 'flex', justifyContent:'center', alignItems:'center'};
+    margin: '1rem 0',
+}
 const Skills = () => {
     return (
         <>
-            <div style={{ height: '10vh'}}/>
-            <Grid container spacing={5} style={{ ...centered,  }}>
-                {logos.map((logo) => (
-                    <Grid item xs={6} sm={6} md={3} key={logo.label}  style={{ ...centered, }}>
-                        <Content label={logo.label} src={logo.src} />
+            <div style={{ height: '10vh' }} />
+            <Grid container spacing={2}>
+                {logos.map((logo, index) => (
+                    // mobile = xs, laptop = md, monitors = lg, everything larger is xl
+                    <Grid item key={logo.label + index} xs={6} sm={4} md={2}>
+                        <Card elevation={20} style={cardStyle}>
+                            <object type="image/svg+xml" data={logo.src} height="50%" width="50%" />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2" fontFamily="monospace">
+                                    {logo.label}
+                                </Typography>
+                            </CardContent>
+                        </Card>
                     </Grid>
                 ))}
             </Grid>
         </>
     );
-}
+};
 export default Skills;
